72df84898f97fa97bd4547d3737bde4ceb1c9bee
Estif Fabian Arenas Pamplona <fabian.arenas@bitsamericas.com>
2022-07-26 17:20:04 -0500

    todo

--- AzureWebApp/Controllers/WeatherForecastController.cs
+++ AzureWebApp/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace AzureWebApp.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "DEVELOP", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
